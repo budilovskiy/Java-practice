285b4a6c65a7da0b19616dbc44fca05129b584e7
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-07-19 20:00:56 +0300

    Update IllegalArrayException.java

--- Exercise1/IllegalArrayException.java
+++ Exercise1/IllegalArrayException.java
@@ -2,12 +2,22 @@ package practice.excercises.excercize1;
 
 public class IllegalArrayException extends Throwable {
 	
+	private String element;
+	
+	public IllegalArrayException(String element) {
+		this.element = element;
+	}
+	
+	public String getElement() {
+		return element;
+	}
+	
 	public String getExceptionMessage(int[] array) {
-		return "Error: Parameters of command-line does not have type \"int\"";
+		return "Error: Parameter " + element + " of command-line does not have type \"int\"";
 	}
 	
 	public String getExceptionMessage(double[] array) {
-		return "Error: Parameters of command-line does not have type \"double\"";
+		return "Error: Parameter " + element + " of command-line does not have type \"double\"";
 	}
 	
 }
