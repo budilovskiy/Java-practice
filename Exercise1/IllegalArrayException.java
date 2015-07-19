dd08a70abbc35034a1115e3710528d03b10d1bc6
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-07-19 20:02:27 +0300

    Update IllegalArrayException.java

--- Exercise1/IllegalArrayException.java
+++ Exercise1/IllegalArrayException.java
@@ -2,12 +2,15 @@ package practice.excercises.excercize1;
 
 public class IllegalArrayException extends Throwable {
 	
+	// An element that causes exception
 	private String element;
 	
+	// Constructor
 	public IllegalArrayException(String element) {
 		this.element = element;
 	}
 	
+	// Getter for element
 	public String getElement() {
 		return element;
 	}
