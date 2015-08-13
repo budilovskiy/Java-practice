74b89f9b355138c4f0deec57f61b993aced51e35
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-08-13 21:59:38 +0300

    Create IllegalAngleException.java

---
+++ Exercise2/IllegalAngleException.java
@@ -0,0 +1,46 @@
+package practice.exercises.exercise1;
+
+@SuppressWarnings("serial")
+class IllegalAngleException extends IllegalArgumentException {
+
+	/**
+	 * Constructs a new IllegalAngleException with no detail message.
+	 */
+	public IllegalAngleException() {
+		super();
+	}
+
+	/**
+	 * Constructs a new IllegalAngleException with the specified detail message.
+	 * 
+	 * @param message
+	 *            the detail message.
+	 */
+	public IllegalAngleException(String message) {
+		super(message);
+	}
+
+	/**
+	 * Constructs a new exception with the specified detail message and cause.
+	 * 
+	 * @param message
+	 *            the detail message
+	 * @param cause
+	 *            the cause
+	 */
+	public IllegalAngleException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	/**
+	 * Constructs a new exception with the specified cause and a detail message
+	 * of (cause==null ? null : cause.toString())
+	 * 
+	 * @param cause
+	 *            the cause
+	 */
+	public IllegalAngleException(Throwable cause) {
+		super(cause);
+	}
+
+}
